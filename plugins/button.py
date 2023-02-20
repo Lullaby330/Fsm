95d47c639c614144fa96797033ab9935402c1838
Lullaby330 <69coder@gmail.com>
2023-02-21 02:25:53 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -10,8 +10,8 @@ def start_button(client):
     if not FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
         buttons = [
             [
-                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
-                InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
+                InlineKeyboardButton(text="Bantuan", callback_data="help"),
+                InlineKeyboardButton(text="Tutup", callback_data="close"),
             ],
         ]
         return buttons
